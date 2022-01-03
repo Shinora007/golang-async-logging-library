f24fff5de7419e58d42f770cda058bd0e84e2ee5
Satya Prakash <b-saprak@microsoft.com>
2022-01-03 21:07:34 +0530

    Shutting Down the Log

--- alog.go
+++ alog.go
@@ -29,20 +29,37 @@ func New(w io.Writer) *Alog {
 		w = os.Stdout
 	}
 	return &Alog{
-		dest:    w,
-		msgCh:   make(chan string),
-		errorCh: make(chan error),
-		m:       &sync.Mutex{},
+		dest:               w,
+		msgCh:              make(chan string),
+		errorCh:            make(chan error),
+		m:                  &sync.Mutex{},
+		shutdownCh:         make(chan struct{}),
+		shutdownCompleteCh: make(chan struct{}),
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-	for message := range al.msgCh {
-		go func(msg string) {
-			al.write(msg, nil)
-		}(message)
+	//for message := range al.msgCh {
+	//	go func(msg string) {
+	//		al.write(msg, nil)
+	//	}(message)
+	//}
+	wg := &sync.WaitGroup{}
+loop:
+	for {
+		select {
+		case msg := <-al.msgCh:
+			go func(msg string, wg *sync.WaitGroup) {
+				wg.Add(1)
+				al.write(msg, wg)
+			}(msg, wg)
+		case _ = <-al.shutdownCh:
+			wg.Wait()
+			al.shutdown()
+			break loop
+		}
 	}
 }
 
@@ -55,7 +72,10 @@ func (al Alog) formatMessage(msg string) string {
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	al.m.Lock()
+
 	defer al.m.Unlock()
+	defer wg.Done()
+
 	if _, err := al.dest.Write([]byte(al.formatMessage(msg))); err != nil {
 		go func() {
 			al.errorCh <- err
@@ -64,6 +84,8 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 }
 
 func (al Alog) shutdown() {
+	close(al.msgCh)
+	al.shutdownCompleteCh <- struct{}{}
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
@@ -81,6 +103,8 @@ func (al Alog) ErrorChannel() <-chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh <- struct{}{}
+	<-al.shutdownCompleteCh
 }
 
 // Write synchronously sends the message to the log output
